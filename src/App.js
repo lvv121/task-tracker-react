0fb7753987c4378a2089a9e19e1107b8a5190a4a
lvv121 <maticzar5@gmail.com>
2021-04-03 14:53:35 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {   
+        id:1,
+        text:'test1',
+        day:'Jan 1st at 1:11am',
+        reminder: true,
+    },
+    {
+        id:2,
+        text:'test2',
+        day:'Feb 2st at 2:22am',
+        reminder: true,
+    },
+    {
+        id:3,
+        text:'test3',
+        day:'Mar 3st at 3:33am',
+        reminder: false,
+    },
+])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+// Delete Task
+const deleteTask =  (id) => {
+ setTasks(tasks.filter((task) => task.id !== id))
 }
 
-export default App;
+ // Toggle Reminder
+ const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) =>
+      task.id === id ? { ...task, reminder: !task.reminder } : task
+    )
+  )
+}
+
+  return (
+    <div className='container'>
+      <Header 
+      onAdd={()=>setShowAddTask
+        (!showAddTask)}
+      showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'no tasks for you'}
+    </div>
+  )
+  
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title,onAdd,showAdd }) => {
+
+    
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button 
+            color = {showAdd ? 'red' : 'green'} 
+            text={showAdd ? 'close' : 'add'} 
+            onClick={onAdd}
+            />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'todo list',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
+
