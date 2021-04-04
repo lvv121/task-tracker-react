6757cac772efc0bdb2b97144b2130847208e1b95
lvv121 <maticzar5@gmail.com>
2021-04-04 02:11:20 +0200

    .

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -13,6 +13,13 @@ const AddTask = ({ onAdd }) => {
       return
     }
 
+    var format = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+
+    if (!day.match(format)) {
+      alert('please write date in good format')
+      return
+    }
+
     onAdd({ text, day, reminder })
 
     setText('')
@@ -35,7 +42,7 @@ const AddTask = ({ onAdd }) => {
         <label></label>
         <input
           type='string'
-          placeholder='add time'
+          placeholder='add time (DD-MM-YYYY format)'
           value={day}
           onChange={(e) => setDay(e.target.value)}
         />
